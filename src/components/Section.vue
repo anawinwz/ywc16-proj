7f8daf6fa069f62b2940bf00c93f2bbf346e86e2
Anawin Wongsadit <imonmac@hotmail.com>
2018-12-24 14:54:03 +0700

    Section Fix

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="section-bg">
-        <div class="container"><slot/></div>
-        
+  <div class="section-bg">
+    <div class="container">
+      <slot/>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-
-    }
+export default {};
 </script>
 <style>
-.section-bg{
-color:white;
-padding:10px;
-background: linear-gradient(168.79deg, #005CEA 5.57%, #40D4FC 96.88%);
+.section-bg {
+  color: white;
+  padding: 10px;
+  background: linear-gradient(168.79deg, #005cea 5.57%, #40d4fc 96.88%);
 }
 </style>
