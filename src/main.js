f98237063d335306f286021719c07842ec5e4439
fluorish <fluorishmp@gmail.com>
2018-12-24 10:02:43 +0700

    depend1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import { routes } from './router/index'
 Vue.config.productionTip = false
+
+const router = new VueRouter({
+	mode: 'history',
+	routes,
+});
+
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,53 @@
+
+// import Store from '@/components/Store'
+// import ProductDetail from '@/components/ProductDetail'
+// import ShoppingCart from '@/components/ShoppingCart'
+// import ProductCategory from '@/components/ProductCategory'
+// import Blog from '@/components/Blog'
+// import BlogDetail from '@/components/subs/BlogDetail'
+// import Login from '@/components/Login'
+// import Logout from '@/components/Logout'
+
+
+// export const routes = [
+// 	{
+// 		path: '/',
+// 		name: 'Store',
+// 		component: Store
+// 	},
+// 	{
+// 		path: '/store/:id',
+// 		name: 'ProductDetail',
+// 		component: ProductDetail
+// 	},
+// 	{
+// 		path: '/cart',
+// 		name: 'ShoppingCart',
+// 		component: ShoppingCart
+// 	},
+// 	{
+// 		path: '/category/:id',
+// 		name: 'ProductCategory',
+// 		component: ProductCategory
+// 	},
+// 	{
+// 		path: '/blog',
+// 		name: 'Blog',
+// 		component: Blog
+// 	},
+// 	{
+// 		path: '/blog/:id',
+// 		name: BlogDetail,
+// 		component: BlogDetail
+// 	},
+// 	{
+// 		path: '/login',
+// 		name: Login,
+// 		component: Login
+// 	},
+// 	{
+// 		path: '/logout',
+// 		name: Logout,
+// 		component: Logout
+// 	}
+// ]
